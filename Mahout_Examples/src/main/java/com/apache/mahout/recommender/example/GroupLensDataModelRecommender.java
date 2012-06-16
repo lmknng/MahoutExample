7c2887ed72186a85f8db65da040f232394f41c8a
Ajit Koti <ajitkoti@gmail.com>
2012-06-16 22:48:30 +0530

    First Commit

---
+++ Mahout_Examples/src/main/java/com/apache/mahout/recommender/example/GroupLensDataModelRecommender.java
@@ -0,0 +1,43 @@
+package com.apache.mahout.recommender.example;
+
+import java.io.File;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.example.grouplens.GroupLensDataModel;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+class GroupLensDataModelRecommender {
+
+  private GroupLensDataModelRecommender() {
+  }
+
+  public static void main(String[] args) throws Exception {
+    DataModel model = new GroupLensDataModel(new File("src/main/resources/ratings.dat"));
+    
+    RecommenderEvaluator evaluator =  new AverageAbsoluteDifferenceRecommenderEvaluator();
+    
+    
+    RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+        @Override
+        public Recommender buildRecommender(DataModel model) throws TasteException {
+          UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+          UserNeighborhood neighborhood = new NearestNUserNeighborhood(100, similarity, model);
+          return new GenericUserBasedRecommender(model, neighborhood, similarity);
+        }
+      };
+    
+   
+      double score = evaluator.evaluate(recommenderBuilder, null, model, 0.95, 0.05);
+      System.out.println(score);
+  }
+
+}
